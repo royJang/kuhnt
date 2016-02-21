fd8630683cd56eef02cc527ca467b21577388135
royJang <zzl1108@hotmail.com>
2016-02-21 23:11:44 +0800

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,20 @@
+module.exports = function (grunt){
+
+    grunt.initConfig({
+        nodewebkit: {
+            options: {
+                platforms: ['win64'],
+                buildDir: './webkitbuilds', // Where the build version of my node-webkit app is saved
+                credits: './bin/Credits.html',
+                macIcns : './icon.icns'
+            },
+            src: ['./bin/**/*'] // Your node-webkit app
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-node-webkit-builder');
+
+    grunt.registerTask('default',[
+        'nodewebkit'
+    ]);
+};
